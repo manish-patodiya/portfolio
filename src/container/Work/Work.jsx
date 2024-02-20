5a949d933974f85a31386ba87d89667b117b0be7
Manish patodiya <mpatodiya2@gmail.com>
2024-02-21 00:08:16 +0530

    add work portfolio section

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
-
+import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import './Work.scss';
 
@@ -11,14 +11,68 @@ const Work = () => {
   const [activeFilter, setActiveFilter] = useState('All');
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
-  // useEffect(() => {
-  //   const query = '*[_type == "works"]';
-
-  //   client.fetch(query).then((data) => {
-  //     setWorks(data);
-  //     setFilterWork(data);
-  //   });
-  // }, []);
+  useEffect(() => {
+        let data = [
+          {
+            title: "Modern UI/UX Website",
+            description: "A Modern UI/UX Portfolio Website",
+            projectLink: "", 
+            codeLink: "",
+            imgUrl: images.about02,
+            tags:["UI/UX","All"],
+          },
+          {
+            title: "Modern UI/UX Website",
+            description: "A Modern UI/UX Portfolio Website",
+            projectLink: "", 
+            codeLink: "",
+            imgUrl: images.about02,
+            tags:["UI/UX","All"],
+          },
+          {
+            title: "Modern UI/UX Website",
+            description: "A Modern UI/UX Portfolio Website",
+            projectLink: "", 
+            codeLink: "",
+            imgUrl: images.about02,
+            tags:["UI/UX","All"],
+          },
+          {
+            title: "Modern UI/UX Website",
+            description: "A Modern UI/UX Portfolio Website",
+            projectLink: "", 
+            codeLink: "",
+            imgUrl: images.about02,
+            tags:["UI/UX","All"],
+          },
+          {
+            title: "Modern UI/UX Website",
+            description: "A Modern UI/UX Portfolio Website",
+            projectLink: "", 
+            codeLink: "",
+            imgUrl: images.about02,
+            tags:["UI/UX","All"],
+          },
+          {
+            title: "Modern UI/UX Website",
+            description: "A Modern UI/UX Portfolio Website",
+            projectLink: "", 
+            codeLink: "",
+            imgUrl: images.about02,
+            tags:["UI/UX","All"],
+          },
+          {
+            title: "Modern UI/UX Website",
+            description: "A Modern UI/UX Portfolio Website",
+            projectLink: "", 
+            codeLink: "",
+            imgUrl: images.about02,
+            tags:["UI/UX","All"],
+          }
+        ];
+        setWorks(data);
+        setFilterWork(data);
+  }, []);
 
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
@@ -40,7 +94,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['UI/UX', 'Web App', 'React JS', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}

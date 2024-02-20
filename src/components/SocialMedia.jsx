4bca830e99eb9ca36a54a59fb7b0388a4482b2ac
Manish patodiya <mpatodiya2@gmail.com>
2024-02-20 23:49:21 +0530

    add about section

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import {BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://www.linkedin.com/in/manish-patodiya" target='_blank'><BsLinkedin /></a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href="https://github.com/manish-patodiya" target='_blank'><BsGithub /></a>
     </div>
     <div>
-      <BsInstagram />
+      <a href="https://www.instagram.com/manish_patodiya__/" target='_blank'><BsInstagram /></a>
     </div>
   </div>
 );
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-
+import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
 
-  // useEffect(() => {
+  useEffect(() => {
+    setAbouts([
+      {title: "Web Development",description:"Turning ideas into reality on the web. I specialize in creating websites that are not only visually appealing but also easy to use and navigate. Let's make your online presence stand out!",imgUrl:images.about01},
+      {title: "Frontend Development",description:"Crafting beautiful, user-friendly websites that make browsing a breeze. I turn designs into interactive experiences that keep users coming back for more.",imgUrl:images.about02},
+      {title: "Backend Developement",description:"Building the backbone of websites with solid databases and efficient server-side logic. I ensure everything runs smoothly behind the scenes, so users can focus on what matters most.",imgUrl:images.about03},
+      {title: "MERN Stack",description:"Bringing together MongoDB, Express.js, React, and Node.js to create dynamic web apps. I love using these tools to build responsive and modern websites that users love to explore.",imgUrl:images.about04}
+    ]);
   //   const query = '*[_type == "abouts"]';
 
   //   client.fetch(query).then((data) => {
   //     setAbouts(data);
   //   });
-  // }, []);
+  }, []);
 
   return (
     <>
-      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+      
+      <h2 className="head-text">I Know that <span>Good Apps</span> <br />means  <span>Good Businesses.</span></h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (

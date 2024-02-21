8753886a19e79169ff45b6c9e18a42fd9c498de7
Manish patodiya <mpatodiya2@gmail.com>
2024-02-21 10:35:44 +0530

    update header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -33,8 +33,9 @@ const Header = () => (
         </div>
 
         <div className="tag-cmp app__flex">
-          <p className="p-text">Web Developer</p>
+          <p className="p-text">Full Stack Developer</p>
           <p className="p-text">Freelancer</p>
+          <p className="p-text">App Developer</p>
         </div>
       </div>
     </motion.div>
@@ -44,7 +45,7 @@ const Header = () => (
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.profile} alt="profile_bg" />
+      {/* <img src={images.profile} alt="profile_bg" /> */}
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: 'easeInOut' }}

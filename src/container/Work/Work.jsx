5f252ae0070e8e26da5c8dff440f7bb2601b1248
Manish patodiya <mpatodiya2@gmail.com>
2024-02-22 22:46:35 +0530

    add projects into portfolio

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,45 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import { AiFillEye, AiFillGithub } from 'react-icons/ai';
-import { motion } from 'framer-motion';
-import { images } from '../../constants';
-import { AppWrap, MotionWrap } from '../../wrapper';
-import './Work.scss';
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import "./Work.scss";
 
 const Work = () => {
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
-  const [activeFilter, setActiveFilter] = useState('All');
+  const [activeFilter, setActiveFilter] = useState("All");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   useEffect(() => {
-        let data = [
-          {
-            title: "Modern UI/UX Website",
-            description: "A Modern UI/UX Portfolio Website",
-            projectLink: "", 
-            codeLink: "",
-            imgUrl: images.about02,
-            tags:["UI/UX","All"],
-          },
-          {
-            title: "Modern UI/UX Website",
-            description: "A Modern UI/UX Portfolio Website",
-            projectLink: "", 
-            codeLink: "",
-            imgUrl: images.about02,
-            tags:["UI/UX","All"],
-          },
-          {
-            title: "Modern UI/UX Website",
-            description: "A Modern UI/UX Portfolio Website",
-            projectLink: "", 
-            codeLink: "",
-            imgUrl: images.about02,
-            tags:["UI/UX","All"],
-          },
-        ];
-        setWorks(data);
-        setFilterWork(data);
+    let data = [
+      {
+        title: "RAAH (Rajasthan Arts and Handicrafts)",
+        description: "E-commerce platform for the Artisans in rajasthan.",
+        projectLink: "",
+        codeLink: "https://github.com/manish-patodiya/raah",
+        imgUrl: images.about02,
+        tags: ["Web App", "All"],
+      },
+      {
+        title: "Invento",
+        description: "Inventory management system for businesses.",
+        projectLink: "",
+        codeLink: "https://github.com/manish-patodiya/Invento",
+        imgUrl: images.about02,
+        tags: ["Web App"],
+      },
+      {
+        title: "E-vaidyak",
+        description:
+          "Healthcare service for patients where they can make appointment and consult with the doctors.",
+        projectLink: "https://evaidyak.com/clinic",
+        codeLink: "",
+        imgUrl: images.about02,
+        tags: ["Web App"],
+      },
+      {
+        title: "Jangir Samaaj",
+        description:
+          "Platform for the jangir society to connect with the society members and keep a track of members in a region",
+        projectLink: "",
+        codeLink: "https://github.com/manish-patodiya/JangirSamaj",
+        imgUrl: images.about02,
+        tags: ["Web App"],
+      },
+      {
+        title: "CVtae",
+        description:
+          "Data mining project for shortlisting resume based on the job role",
+        projectLink: "",
+        codeLink: "https://github.com/manish-patodiya/CVtae",
+        imgUrl: images.about02,
+        tags: ["Web App"],
+      },
+      {
+        title: "Modern UI/UX Website",
+        description: "A Modern UI/UX Portfolio Website.",
+        projectLink: "",
+        codeLink: "https://github.com/manish-patodiya/portfolio",
+        imgUrl: images.about02,
+        tags: ["UI/UX", "React JS"],
+      },
+      {
+        title: "Calc",
+        description:
+          "Calculator clone made using Java AWT and Swing Components",
+        projectLink: "",
+        codeLink: "",
+        imgUrl: images.about02,
+        tags: ["UI/UX"],
+      },
+      {
+        title: "Story club",
+        description: "Frontend UI in react",
+        projectLink: "https://manish-patodiya.github.io/storyclub/",
+        codeLink: "https://github.com/manish-patodiya/storyclub",
+        imgUrl: images.about02,
+        tags: ["UI/UX", "React JS"],
+      },
+    ];
+
+    setWorks(data);
+    setFilterWork(data);
   }, []);
 
   const handleWorkFilter = (item) => {
@@ -49,7 +94,7 @@ const Work = () => {
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if (item === 'All') {
+      if (item === "All") {
         setFilterWork(works);
       } else {
         setFilterWork(works.filter((work) => work.tags.includes(item)));
@@ -59,14 +104,18 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+      <h2 className="head-text">
+        My Creative <span>Portfolio</span> Section
+      </h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'React JS', 'All'].map((item, index) => (
+        {["UI/UX", "Web App", "React JS", "All"].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
-            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
           >
             {item}
           </div>
@@ -80,43 +129,55 @@ const Work = () => {
       >
         {filterWork.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
-            <div
-              className="app__work-img app__flex"
-            >
+            <div className="app__work-img app__flex">
               <img src={work.imgUrl} alt={work.name} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                {work.projectLink ? (
+                  <a href={work.projectLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                ) : (
+                  ""
+                )}
 
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillEye />
-                  </motion.div>
-                </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
+                {work.codeLink ? (
+                  <a href={work.codeLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                ) : (
+                  ""
+                )}
               </motion.div>
             </div>
 
             <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
 
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
@@ -130,7 +191,7 @@ const Work = () => {
 };
 
 export default AppWrap(
-  MotionWrap(Work, 'app__works'),
-  'work',
-  'app__primarybg',
+  MotionWrap(Work, "app__works"),
+  "work",
+  "app__primarybg"
 );
